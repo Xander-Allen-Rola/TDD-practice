074ed1021b062d5048f9f77f69686f8cc938f330
Xander Allen Rola <20190016434@my.xu.edu.ph>
2024-03-13 01:00:03 +0800

    Added assertNoteEqual and asserEqual(faile) test cases in addition

--- main.py
+++ main.py
@@ -4,13 +4,15 @@ import math
 class TestCalculator(unittest.TestCase):
     def test_add(self):
         self.assertEqual(add(2, 3), 5)
+        self.assertNotEqual(add(1, 1), 1)
+        self.assertEqual(add(1), 2)
 
     def test_divide(self):
         with self.assertRaises(ZeroDivisionError):
             divide(10,0)
 
     def test_multiply(self):
-	self.assertEqual(multiply(3, 5), 15)		
+	    self.assertEqual(multiply(3, 5), 15)
 
     def test_subtract(self):
         self.assertEqual(subtract(3,2), 1)
